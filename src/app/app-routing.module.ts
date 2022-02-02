56ba4aa2e734de0e8c6ede44c9985494692bbe10
Tetiana Skazhenyk <tianati01@gmail.com>
2022-02-02 23:07:36 +0200

    added dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutContainerComponent } from './modules/core/layout/layout-container/layout-container.component';
+import { DashboardComponent } from './modules/dashboard/components/dashboard/dashboard.component';
 import { ProductsComponent } from './modules/products/components/products/products.component';
 
 const routes: Routes = [
     {path: '', component: LayoutContainerComponent, children: [
-      { path: 'products', component: ProductsComponent, loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule)}
+      { path: 'products', component: ProductsComponent, loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule) },
+      { path: 'dashboard', component: DashboardComponent, loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }
     ]}
   ];
 
